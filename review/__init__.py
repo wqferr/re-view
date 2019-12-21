faf835911f7abe04b3b161fbe8755d9f5d01a3e1
William Ferreira <wqferr@gmail.com>
2019-12-21 20:52:16 -0300

    Create setup.py

---
+++ review/__init__.py
@@ -0,0 +1,2 @@
+"""Terminal based regex visualization tool and debugger."""
+__version__ = "0.0.0"
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""Install minimal requirements."""
+import os
+
+from setuptools import find_packages, setup
+
+from review import __doc__ as project_short_desc
+from review import __version__ as project_version
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def _read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="re-view",
+        version=project_version,
+        author="William Quelho Ferreira",
+        author_email="wqferr@gmail.com",
+        description=project_short_desc,
+        license="GPLv3",
+        keywords="regex visualization tool debug",
+        url="https://github.com/wqferr/re-view",
+        packages=find_packages(),
+        long_description=_read("README.md"),
+        # TODO add more classifiers
+        classifiers=["Development Status :: 3 - Alpha", "Topic :: Utilities"],
+    )
