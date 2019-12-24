5eaef91bad2858a4c768f5bc6298d7509854449f
William Ferreira <wqferr@gmail.com>
2019-12-23 22:00:36 -0300

    Some fooling around with highlighting

--- review/app.py
+++ review/app.py
@@ -1,11 +1,33 @@
+import re
+from sys import stdin
+
 from blessed import Terminal
+from lorem.text import TextLorem
+
+RAW_LOREM = TextLorem(prange=(2, 2), psep="").text()
+# WRAPPED_LOREM = re.sub(r"", r"\1\n", RAW_LOREM)
+
+
+def echo(*args):
+    """Print with no newline and flush immediately."""
+    print(*args, end="", flush=True)
 
 
 def main():
-    """Execute tests."""
+    """Fooling around."""
     term = Terminal()
-    print(term.bold("hello!"))
-    print(term.bold(term.underline("HELLO")))
+
+    def _highlight_match(m):
+        return term.bold(term.underline(m.group(0)))
+
+    with term.fullscreen(), term.cbreak():
+        while True:
+            echo(term.move_y(term.height // 2))
+            for line in term.wrap(RAW_LOREM, width=80):
+                print(re.sub(r"(i[^it]+t)", _highlight_match, line))
+            key = stdin.read(1)
+            if key == "q":
+                break
 
 
 if __name__ == "__main__":
