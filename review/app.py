f6849278ed80b2aeabd4f68ef93117f01dc04fbe
William Ferreira <wqferr@gmail.com>
2019-12-23 23:36:18 -0300

    Implement rudimentary real-time editing

--- review/app.py
+++ review/app.py
@@ -24,20 +24,44 @@ def main():
         return term.bold(term.underline(m.group(0)))
 
     with term.fullscreen(), term.cbreak():
-        key = None
+        if len(argv) > 1:
+            cur_regex = argv[1]
+        else:
+            cur_regex = ""
+
         while True:
             echo(term.clear)
-            echo(key)
             echo(term.move_y(term.height // 2))
-            highlighted_lorem = re.sub(
-                argv[1], _highlight_match, RAW_LOREM, flags=re.DOTALL
-            )
-            for line in term.wrap(highlighted_lorem, width=term.width - 2 * margin):
+            invalid_regex = False
+            if cur_regex:
+                try:
+                    highlighted_lorem = re.sub(
+                        cur_regex, _highlight_match, RAW_LOREM, flags=re.DOTALL
+                    )
+                    # highlighted_lorem = RAW_LOREM
+                except:
+                    invalid_regex = True
+            else:
+                invalid_regex = True
+
+            if invalid_regex:
+                highlighted_lorem = RAW_LOREM
+
+            wrapped = term.wrap(highlighted_lorem, width=term.width - 2 * margin)
+            echo(term.move_y((term.height - len(wrapped)) // 2))
+            for line in wrapped:
                 echo(term.move_x(margin))
                 print(line)
-            key = stdin.read(1)
-            if key == "q":
-                break
+
+            echo(term.move_y(term.height - 1))
+            echo(cur_regex)
+            key = term.inkey()
+            if key.is_sequence:
+                if key.name == "KEY_DELETE":
+                    cur_regex = cur_regex[:-1]
+            elif key.isascii:
+                cur_regex = cur_regex + key
+    print("GOT ", key, ord(key), key.is_sequence, str(key), key.name)
 
 
 if __name__ == "__main__":
