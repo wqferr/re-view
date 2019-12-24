2cbc8af0d1622a5084fd1fd41329d8817f6770d9
William Ferreira <wqferr@gmail.com>
2019-12-23 23:09:34 -0300

    Add margin

--- review/app.py
+++ review/app.py
@@ -5,7 +5,8 @@ from sys import argv, stdin
 from blessed import Terminal
 from lorem.text import TextLorem
 
-RAW_LOREM = TextLorem(prange=(2, 2)).text()
+# RAW_LOREM = TextLorem(trange=(2, 3), prange=(2, 3), srange=(6, 6)).text()
+RAW_LOREM = TextLorem().text()
 # WRAPPED_LOREM = re.sub(r"", r"\1\n", RAW_LOREM)
 
 
@@ -17,17 +18,22 @@ def echo(*args):
 def main():
     """Fooling around."""
     term = Terminal()
+    margin = 2
 
     def _highlight_match(m):
         return term.bold(term.underline(m.group(0)))
 
     with term.fullscreen(), term.cbreak():
+        key = None
         while True:
-            print(term.move(term.height // 2), 0)
+            echo(term.clear)
+            echo(key)
+            echo(term.move_y(term.height // 2))
             highlighted_lorem = re.sub(
                 argv[1], _highlight_match, RAW_LOREM, flags=re.DOTALL
             )
-            for line in term.wrap(highlighted_lorem, width=20):
+            for line in term.wrap(highlighted_lorem, width=term.width - 2 * margin):
+                echo(term.move_x(margin))
                 print(line)
             key = stdin.read(1)
             if key == "q":
