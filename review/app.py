1766e3c9eb8195a883b58ed25b1746a5e76bf9fa
William Ferreira <wqferr@gmail.com>
2019-12-24 19:43:52 -0300

    Organize things into a class

--- review/app.py
+++ review/app.py
@@ -1,77 +1,105 @@
 """TUI tool to visualize regular expressions in real time."""
 import re
-from sys import argv
 
 from blessed import Terminal
 from lorem.text import TextLorem
 
-RAW_LOREM = TextLorem().text()
+LOREM_GENERATOR = TextLorem()
 
 
 def echo(*args):
-    """Print with no newline and flush immediately."""
-    print(*args, end="", flush=True)
+    """Equivalent to print with no end and sep strings.
+
+    Also flushes immediately.
+    """
+    print(*args, end="", sep="", flush=True)
 
 
-def _get_highlighted_text(text, regex, highlight_function):
-    if not regex:
-        return text
-    try:
-        highlighted_text = re.sub(regex, highlight_function, text)
-    except re.error:
-        return text
-    else:
-        return highlighted_text
+class Application:
+    """Fullscreen application."""
 
-
-def _process_key(term, cur_regex):
-    key = term.inkey()
-
-    if key.is_sequence:
-        if key.name == "KEY_DELETE":
-            cur_regex = cur_regex[:-1]
-    elif key.isascii:
-        if key == "\x0c":
-            print(term.clear)
+    def __init__(self, *, margin=2, initial_regex="", text=None):
+        """See doc(Application)."""
+        self.term = Terminal()
+        self.margin = margin
+        self.regex = initial_regex
+        if text is None:
+            self.text = LOREM_GENERATOR.text()
         else:
-            cur_regex = cur_regex + key
-    return cur_regex
+            self.text = text
+
+    def _get_highlighted_text(self):
+        if not self.regex:
+            return self.text
+
+        try:
+            return re.sub(self.regex, self._highlight_match, self.text)
+        except re.error:
+            return self.text
+
+    def _process_key(self):
+        key = self.term.inkey()
+
+        if key.is_sequence:
+            if key.name == "KEY_DELETE":
+                self.regex = self.regex[:-1]
+        elif key.isascii:
+            if key == "\x0c":
+                echo(self.term.clear)
+            else:
+                self.regex = self.regex + key
+        return self.regex
+
+    def _highlight_match(self, m):
+        matched_text = m.group(0)
+        if not matched_text:
+            return matched_text
+        else:
+            return self.term.bold_underline_reverse(matched_text)
+
+    def _move(self, x=None, y=None):
+        if x:
+            echo(self.term.move_x(x))
+        if y:
+            echo(self.term.move_y(y))
+
+    def _print_text(self):
+        highlighted_text = self._get_highlighted_text()
+        wrapped = self.term.wrap(
+            highlighted_text,
+            width=self.term.width - 2 * self.margin,
+            subsequent_indent="> ",
+        )
+        self._move(y=(self.term.height - len(wrapped)) // 2)
+        for line in wrapped:
+            self._move(x=self.margin)
+            echo(line + "\n")
+
+    def _print_regex(self):
+        self._move(y=self.term.height - 1)
+        echo(self.regex + self.term.clear_eol)
+
+    def run(self):
+        """Start fullscreen application.
+
+        Returns on KeyboardInterrupt.
+        """
+        # FIXME
+        with self.term.fullscreen(), self.term.cbreak():
+            while True:
+                self._print_text()
+                self._print_regex()
+
+                try:
+                    self._process_key()
+                except KeyboardInterrupt:
+                    break
 
 
 def main():
     """Fooling around."""
-    term = Terminal()
-    margin = 2
-
-    def _highlight_match(m):
-        return term.bold(term.underline(m.group(0)))
-
-    if len(argv) > 1:
-        cur_regex = argv[1]
-    else:
-        cur_regex = ""
-
-    with term.fullscreen(), term.cbreak():
-        while True:
-            highlighted_lorem = _get_highlighted_text(
-                RAW_LOREM, cur_regex, _highlight_match
-            )
-
-            wrapped = term.wrap(
-                highlighted_lorem, width=term.width - 2 * margin, subsequent_indent="> "
-            )
-            echo(term.move_y((term.height - len(wrapped)) // 2))
-            for line in wrapped:
-                echo(term.move_x(margin))
-                print(line)
-
-            echo(term.move_y(term.height - 1))
-            echo(cur_regex + term.clear_eol)
-
-            try:
-                cur_regex = _process_key(term, cur_regex)
-            except KeyboardInterrupt:
-                break
+    app = Application()
+    app.run()
 
 
 if __name__ == "__main__":
